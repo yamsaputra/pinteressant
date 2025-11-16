b80079573a65ef9b781ffb50fd7b88f18a669e58
yamsaputra <yam_1304@ymail.com>
2025-11-16 20:26:08 +0100

    Backend folder created.

---
+++ backend/server.mjs
@@ -0,0 +1,17 @@
+// Import statements
+import express from 'express';
+
+// Initialize Express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+// GET Routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
